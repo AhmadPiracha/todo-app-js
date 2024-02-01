a7add7ea0a137b4303ef51d161b4aed7a9ae5644
Ahmad Piracha <ahmadpiracha3@gmail.com>
2024-02-01 15:09:11 +0500

    code updation using jquery concepts

--- script.js
+++ script.js
@@ -1,137 +1,157 @@
 class Task {
-    static lastId = 0;
-    constructor(title, description, dueDate, status) {
-        this.id = Task.lastId + 1;
-        this.title = title;
-        this.description = description;
-        this.dueDate = dueDate;
-        this.status = status;
-        Task.lastId += 1;
-    }
+  static lastId = 0;
+  constructor(title, description, dueDate, status) {
+    this.id = Task.lastId + 1;
+    this.title = title;
+    this.description = description;
+    this.dueDate = dueDate;
+    this.status = status;
+    Task.lastId += 1;
+  }
 }
 const tasks = [];
 
 function AddTask() {
-    var taskTitle = document.getElementById("title").value;
-    var taskDescription = document.getElementById("description").value;
-    var current_date = new Date();
-    var taskDueDate = document.getElementById("dueDate").value;
-    var date = new Date(taskDueDate);
-    var taskStatus = document.getElementById("status").value;
+  var taskTitle = $("#title").val();
+  var taskDescription = $("#description").val();
+  var current_date = new Date();
+  var taskDueDate = $("#dueDate").val();
+  var date = new Date(taskDueDate);
+  var taskStatus = $("#status").val();
 
-    if (
-        taskTitle.trim() === "" ||
-        taskDescription.trim() === "" ||
-        taskDueDate.trim() === ""
-    ) {
-        alert("Fields can't be Empty...");
-        return;
-    }
-    if (date < current_date) {
-        alert(
-            "Invalid date or date is in the past. Please enter a valid date"
-        );
-        return;
-    }
+  if (taskTitle.trim() === "") {
+    alert("Title can not Empty");
+    return;
+  }
 
+  if (taskDescription.trim() === "") {
+    alert("Description can not Empty");
+    return;
+  }
 
-    const taskObj = new Task(taskTitle, taskDescription, taskDueDate, taskStatus);
+  if (taskDueDate.trim() === "") {
+    alert("Date can not Empty");
+    return;
+  }
 
-    // Adding data in List
-    tasks.push(taskObj);
-    // Displaying data From lIst
-    displayData(taskObj);
-    document.getElementById("title").value = "";
-    document.getElementById("description").value = "";
-    document.getElementById("dueDate").value = "";
-    document.getElementById("status").value = "Set Status";
+  if (taskStatus === "Set Status") {
+    alert("Invalid Status");
+    return;
+  }
+
+  if (date < current_date) {
+    alert(
+      "Invalid date or date is in the past. Please enter a valid date"
+    );
+    return;
+  }
+  const taskObj = new Task(
+    taskTitle,
+    taskDescription,
+    taskDueDate,
+    taskStatus
+  );
+
+  // Adding data in List
+  tasks.push(taskObj);
+  // Displaying data From lIst
+  displayData(taskObj);
+
+  $("#title, #description, #dueDate").val("");
+  $("#status").val("Set Status");
 }
 
-function displayData(taskList) {
-    // console.log(taskList);
+function displayData(t) {
+  console.log(t);
+  var output = $("#taskList");
+  console.log("output", output);
+  var existingRow = $(`#${t.id}`);
+  // var existingRow = $("#"+t.id);
 
-    var output = document.getElementById("taskList");
-    const existingRow = document.getElementById(`${taskList.id}`);
-
-    if (existingRow) {
-        existingRow.innerHTML = `
-    <td>${taskList.id}</td>
-    <td>${taskList.title}</td>
-    <td>${taskList.description}</td>
-    <td>${taskList.dueDate}</td>
-    <td>${taskList.status}</td>
+  if (existingRow.length) {
+    existingRow.html(`
+    <td>${t.id}</td>
+    <td>${t.title}</td>
+    <td>${t.description}</td>
+    <td>${t.dueDate}</td>
+    <td>${t.status}</td>
     <td>
-    <input class="form-check-input" type="checkbox" id="${
-      taskList.id
-    }" name="${taskList.title}" value="${
-    taskList.status
-  }" onclick="markTask(${taskList.id})" 
-      ${taskList.status === "Complete" ? "checked" : ""} 
-      ${taskList.status === "Complete"
-      ? existingRow.classList.add("complete")
-      : existingRow.classList.remove("complete")
-  } ${taskList.status === "Complete" ? disabled="disabled" : ""} />
-  </td>
-
-  <td><button class="btn btn-danger" id="${
-    taskList.id
-  }" type="button" onclick="deleteTask(${taskList.id});"${
-    taskList.status === "Pending" ? "disabled" : ""
-  }>Delete</button></td>`;
-    } else {
-        const newRow = document.createElement("tr");
-        newRow.id = `${taskList.id}`;
-
-        newRow.innerHTML = `
-  <td>${taskList.id}</td>
-  <td>${taskList.title}</td>
-  <td>${taskList.description}</td>
-  <td>${taskList.dueDate}</td>
-  <td>${taskList.status}</td>
-  <td>
-    <input class="form-check-input" type="checkbox" id="${
-      taskList.id
-    }" name="${taskList.title}" value="${
-    taskList.status
-  }" onclick="markTask(${taskList.id})" ${
-    taskList.status === "Complete" ? "checked" : ""
-  } ${taskList.status === "Complete" ? disabled="disabled" : ""} />
-  </td>
-  <td>
-    <button class="btn btn-danger" id="${
-      taskList.id
-    }" type="button" onclick="deleteTask(${taskList.id});" ${
-    taskList.status === "Pending" ? "disabled" : ""
-  }>Delete</button>
-  </td>`;
-
-        output.appendChild(newRow);
-
-        taskList.status === "Complete" ?
-            newRow.classList.add("complete") :
-            "";
+      <input class="form-check-input" type="checkbox" id="${
+        t.id
+      }" name="${t.title}" value="${t.status}" onclick="markTask(${
+      t.id
+    })"  ${
+      t.status === "Complete"
+        ? existingRow.addClass("complete")
+        : existingRow.removeClass("complete")
     }
+        ${t.status === "Complete" ? "checked" : ""}
+        ${t.status === "Complete" ? (disabled = "disabled") : ""}
+      />
+
+    </td>
+    <td>
+      <button class="btn btn-danger" id="${
+        t.id
+      }" type="button" onclick="deleteTask(${t.id});"
+      ${t.status === "Pending" ? "disabled" : ""}
+      >Delete</button></td>`);
+  } else {
+    const newRow = $("<tr></tr>");
+    newRow.attr("id", `${t.id}`);
+    newRow.html(`
+    <td>${t.id}</td>
+    <td>${t.title}</td>
+    <td>${t.description}</td>
+    <td>${t.dueDate}</td>
+    <td>${t.status}</td>
+    <td>
+      <input class="form-check-input" type="checkbox" id="${
+        t.id
+      }" name="${t.title}" value="${t.status}" onclick="markTask(${
+      t.id
+    })" 
+        ${t.status === "Complete" ? "checked" : ""}
+        ${t.status === "Complete" ? (disabled = "disabled") : ""}
+      />
+    </td>
+    <td>
+      <button class="btn btn-danger" id="${
+        t.id
+      }" type="button" onclick="deleteTask(${t.id});" 
+      ${t.status === "Pending" ? "disabled" : ""}
+      >Delete</button>
+    </td>`);
+
+    output.append(newRow);
+
+    t.status === "Complete"
+      ? newRow.addClass("complete")
+      : "";
+  }
 }
 
 function deleteTask(task_id) {
-    if (tasks.id !== -1) {
-        tasks.splice(tasks.id, 1);
-        const deleteTaskList = document.getElementById(`${task_id}`);
-        if (deleteTaskList) {
-            deleteTaskList.remove();
-        }
+  console.log("Delete Task Called");
+  if (tasks.id !== -1) {
+    tasks.splice(tasks.id, 1);
+    const deleteTaskList = document.getElementById(`${task_id}`);
+
+    if (deleteTaskList) {
+      deleteTaskList.remove();
     }
+  }
 }
 
 function markTask(task_id) {
-    var task = tasks.find((task) => task.id === task_id);
-    var checkbox = document.getElementById(task_id);
+  var task = tasks.find((task) => task.id === task_id);
+  var checkbox = document.getElementById(task_id);
 
-    if (!checkbox.checked) {
-        task.status = "Complete";
-    } else {
-        alert("Completed Task can't be changed");
-    }
+  if (!checkbox.checked) {
+    task.status = "Complete";
+  } else {
+    alert("Completed Task can't be changed");
+  }
 
-    displayData(task);
+  displayData(task);
 }
